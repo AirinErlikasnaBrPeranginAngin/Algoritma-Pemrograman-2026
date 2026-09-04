954b1ce37b31bd8c2b6a4cc9b201c24ee3b28af1
Airin Erlikasna Br Perangin Angin <airinerli8@email.com>
2026-09-04 18:07:39 +0700

    tugas pertemuan 1

---
+++ Pertemuan-01/tugas1.py
@@ -0,0 +1,12 @@
+matematika = 80
+bahasa_indonesia = 75
+ipa = 85
+rata_rata = (matematika + bahasa_indonesia + ipa) / 3
+print("Nilai Matematika =", matematika)
+print("Nilai Bahasa Indonesia =", bahasa_indonesia)
+print("Nilai IPA =", ipa)
+print("Nilai rata-rata =", rata_rata)s
+if rata_rata >= 75:
+    print("Status: LULUS")
+else:
+    print("Status: TIDAK LULUS") 
